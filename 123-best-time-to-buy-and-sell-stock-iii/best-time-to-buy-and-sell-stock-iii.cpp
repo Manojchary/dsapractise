2d33c9c0efaeb2f04e867a27a47a5f4789a675fd
MANOJ GOTTALA <108991891+Manojchary@users.noreply.github.com>
2026-09-09 11:51:23 +0530

    Time: 195 ms (53.59%) | Memory: 140.1 MB (55.16%) - LeetSync

--- 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,43 +1,27 @@
 class Solution {
 public:
-    int solve(int idx , int tobuy  , int cap , vector<int>&prices , int n ,  vector<vector<vector<int>>>&dp){
-        if(idx==n || cap==0) return 0;
+    int solve(int idx ,  int tranNo  , int k ,  vector<int>&prices , int n ,  vector<vector<int>>&dp){
+        if(idx==n || tranNo==k) return 0;
 
-        if(dp[idx][tobuy][cap]!=-1) dp[idx][tobuy][cap];
+        if(dp[idx][tranNo]!=-1) return dp[idx][tranNo];
+
+        int tobuy = tranNo%2;
         int profit = 0;
-        if(tobuy){
-            profit = max(-prices[idx]+solve(idx+1 , 0 , cap ,  prices , n , dp ) , 0+solve(idx+1, 1 , cap, prices , n , dp));
+        if(!tobuy){
+            profit = max(-prices[idx]+solve(idx+1 , tranNo+1 , k ,  prices , n , dp ) , 0+solve(idx+1, tranNo , k , prices , n , dp));// max( vuy this , notbuy this)
 
         }else{
-            profit = max(prices[idx]+solve(idx+1 , 1 , cap-1 , prices , n , dp) , 0+solve(idx+1 , 0 , cap , prices , n , dp));
+            profit = max(prices[idx]+solve(idx+1 , tranNo+1 , k , prices , n , dp) , 0+solve(idx+1 , tranNo , k , prices , n , dp));
         }
 
-        return dp[idx][tobuy][cap] = profit;
+        return dp[idx][tranNo] = profit;
+        
     }
     int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-        //vector<vector<vector<int>>>dp(n+1 , vector<vector<int>>(2 , vector<int>(3 , 0)));
         
-        vector<vector<int>>after(2 , vector<int>(3 , 0));
-        vector<vector<int>>curr(2 , vector<int>(3 , 0));
-
-        for(int idx = n-1 ; idx>=0 ; idx--){
-            for(int tobuy = 0 ; tobuy<=1 ; tobuy++){
-                for(int cap = 1 ; cap<=2 ; cap++){
-
-                    int profit = 0;
-                    if(tobuy){
-                        profit = max(-prices[idx]+after[0][cap] , 0+after[1][cap]);
-                    }else{
-                        profit = max(prices[idx]+after[1][cap-1] , 0+after[0][cap]);
-                    }
-
-                    curr[tobuy][cap] = profit;
-
-                }
-            }
-            after = curr;
-        }
-        return after[1][2];
+        vector<vector<int>>dp(prices.size() ,vector<int>(4 , -1) );
+        return solve(0, 0 , 4 , prices ,  prices.size() , dp );// 2 means 4 trasation buy , sell , buy , sell ;
+        
+        
     }
 };
