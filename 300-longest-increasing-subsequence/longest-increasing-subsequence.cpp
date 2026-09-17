0ddffbd475080cb62aa0663738ffd5921ca2b647
MANOJ GOTTALA <108991891+Manojchary@users.noreply.github.com>
2026-09-17 23:16:55 +0530

    Time: 492 ms (24.45%) | Memory: 296.8 MB (6.92%) - LeetSync

---
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    // using recursion with idx format take and not take by also taking comination of prev and curr idx ,
+    int solve(int prev , int idx , vector<int>&nums , int n  , vector<vector<int>>&dp){
+
+        if(idx>=n ) return 0;
+
+        if(dp[prev+1][idx]!=-1) return dp[prev+1][idx];
+        // not take 
+        
+        int nottake = 0+solve(prev , idx+1 , nums , n , dp);
+
+        //take
+        int take = 0;
+
+        if(prev==-1 || nums[prev]<nums[idx]) take = 1+solve(idx , idx+1 , nums , n , dp);/// we took that prev so curr idx is the next prev;
+
+        return dp[prev+1][idx] = max(nottake , take);
+
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        int n = nums.size();
+
+        vector<vector<int>>dp(n+1 , vector<int>(n+1 , -1));
+
+        return solve(-1 , 0 , nums , nums.size() , dp);
+
+    }
+};
