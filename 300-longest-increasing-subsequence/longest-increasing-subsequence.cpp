377914949c6175963da728fbea16c910b0973fd1
MANOJ GOTTALA <108991891+Manojchary@users.noreply.github.com>
2026-09-17 23:32:38 +0530

    Time: 292 ms (30.22%) | Memory: 296.5 MB (21.75%) - LeetSync

--- 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/longest-increasing-subsequence.cpp
@@ -21,9 +21,26 @@ public:
     int lengthOfLIS(vector<int>& nums) {
         int n = nums.size();
 
-        vector<vector<int>>dp(n+1 , vector<int>(n+1 , -1));
+        vector<vector<int>>dp(n+1 , vector<int>(n+1 , 0));
 
-        return solve(-1 , 0 , nums , nums.size() , dp);
+        //return solve(-1 , 0 , nums , nums.size() , dp);
+
+        //solving using tabulation
+
+        for(int idx = n-1 ; idx>=0 ; idx--){
+            for(int prev = -1 ; prev<idx ; prev++){
+                int nottake = dp[idx+1][prev+1];
+
+                int take = 0;
+
+                if(prev==-1 || nums[prev]<nums[idx]) take = 1+ dp[idx+1][idx+1];
+
+                dp[idx][prev+1] = max(take , nottake);
+            }
+        }
+
+        return dp[0][0];
+
 
     }
 };
