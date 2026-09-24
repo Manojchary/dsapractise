e4245730fcb3a81ed7e3ce3a09a507d80fd5fdfc
MANOJ GOTTALA <108991891+Manojchary@users.noreply.github.com>
2026-09-24 23:34:46 +0530

    Time: 201 ms (9.41%) | Memory: 23.7 MB (5.09%) - LeetSync

---
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    long long solve(int i, int n, vector<int>& nums, vector<long long>& dp) {
+        // Already at or beyond the last index
+        if (i >= n - 1)
+            return 0;
+
+        // Already calculated
+        if (dp[i] != -1)
+            return dp[i];
+
+        long long minJumps = INT_MAX;
+
+        // Try every position we can jump to
+        for (int j = i + 1; j <= min(n - 1, i + nums[i]); j++) {
+
+            long long jumps = 1 + solve(j, n, nums, dp);
+
+            minJumps = min(minJumps, jumps);
+        }
+
+        return dp[i] = minJumps;
+    }
+
+    int jump(vector<int>& nums) {
+        int n = nums.size();
+
+        if (n == 1)
+            return 0;
+
+        vector<long long> dp(n, -1);
+
+        return solve(0, n, nums, dp);
+    }
+};
